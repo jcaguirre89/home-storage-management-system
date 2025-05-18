cadc94c5edc4605bd9f7e6b2353fc345e2b20c05
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2025-05-18 14:57:48 -0400

    update

--- my-app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'], // Ensure this path is correct for my-app/src
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
