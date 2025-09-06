bcd4c622cbcfcb2e9a064dc1b80bf8cdde683e08
Cristobal <cristobal.aguirre89@gmail.com>
2025-09-06 10:46:30 -0400

    update

--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -55,9 +55,6 @@ const Register: React.FC<RegisterProps> = ({ onToggle }) => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Register now!</h1>
-        </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form className="card-body" onSubmit={handleSubmit}>
             <div className="form-control">
